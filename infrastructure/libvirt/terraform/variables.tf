de639ea82ff80fe2c9581ff51e0f27ed996f5e1d
somatczk <soma.toczik@marshmallow.co>
2025-05-31 16:58:16 +0200

    added changes

--- infrastructure/libvirt/terraform/variables.tf
+++ infrastructure/libvirt/terraform/variables.tf
@@ -12,3 +12,9 @@ variable "ssh_public_key_path" {
   type        = string
   default     = "~/.ssh/id_rsa.pub"
 }
+
+variable "base_image_url" {
+  description = "URL of the base image"
+  type        = string
+  default     = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.qcow2"
+}
